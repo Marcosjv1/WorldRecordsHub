a312c4ebb36deaa5f68485afd5b4deb3eea32368
Marcos <marcos.javier.vidal@gmail.com>
2026-02-26 20:22:54 -0600

    quité el hash raro del nombre del archivo al compilar

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/worldrecords.js',
+        chunkFileNames: 'assets/worldrecords-[name].js',
+        assetFileNames: 'assets/worldrecords.[ext]',
+      },
+    },
+  },
 })
